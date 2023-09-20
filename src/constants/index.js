4f4d478e49d91ace096e67d4dac6f751931f4951
RahulAgg0307 <rahul@khidki.homes>
2023-09-20 17:59:15 +0530

    initial_commit

---
+++ src/constants/index.js
@@ -0,0 +1,239 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    carrent,
+    jobit,
+    tripguide,
+    nextjs,
+    ericsson,
+    khidki,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React.js Developer",
+      icon: mobile,
+    },
+    {
+      title: "Python Developer",
+      icon: backend,
+    },
+   
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "nextjs",
+      icon: nextjs,
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Software Developer",
+      company_name: "Khidki Homes",
+      icon: khidki,
+      iconBg: "#383E56",
+      date: "Aug 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+        "I played a pivotal role in supporting our marketing team by utilizing data scraping techniques within Google Maps to gather valuable lead data. ",
+        "This initiative not only helped streamline our lead generation efforts but also contributed significantly to our marketing campaigns success.",
+        "I orchestrated the successful implementation of the WhatsApp API to revolutionize our company's communication strategy. This strategic move not only facilitated seamless interactions with our clients but also enabled us to effortlessly send real-time updates such as invoices and order confirmations, enhancing our overall customer experience.",
+      ],
+    },
+    {
+      title: "Intern",
+      company_name: "Ericsson",
+      icon: ericsson,
+      iconBg: "#E6DEDD",
+      date: "June 2023 - Aug 2023",
+      points: [
+        "I had the privilege to be part of a significant project called 'Rosetta Platform Migration.",
+        "The project involved migrating the servers involved in the automation solution platform.",
+        "My key role was ensuring the secure migration of critical data.",
+        "This experience allowed me to strengthen my skills in data security, server migration, and cross-border operations.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Khidki Homes",
+      icon: khidki,
+      iconBg: "#383E56",
+      date: "May 2022 - Dec 2022",
+      points: [
+        "Responsible for making website using HTML, CSS, Bootstrap, Javascript, APIs like Google script and Whatsapp.",
+        "Creating new pages, adding new functionality to the website, overseeing website redesigns. including troubleshooting problems with website, creating graphic assets and social media content.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
